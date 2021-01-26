7b4b7d2076d4021dc87c74da8189e8647a05e720
mateusgomes01 <mgm6@cin.ufpe.br>
2021-01-26 12:34:13 -0300

    routes folder created

--- server/index.js
+++ server/index.js
@@ -3,9 +3,13 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-require('dotenv').config()
+import postRoutes from './routes/posts.js';
+
+require('dotenv').config();
 
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
