c18bf5c2021ecb37a441fedeae10de2095b1ae81
mateusgomes01 <mgm6@cin.ufpe.br>
2021-01-28 17:10:19 -0300

    adds styles + axios

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,16 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import memories from './images/memories.png'
+import useStyles from "./styles";
 
 const App = () => {
+    const classes = useStyles();
+
     return (
         <Container maxwidth="lg">
-            <AppBar pisition="static" color="inherit">
-                <Typography variant="h2" align="center">Memories</Typography>
-                <img src={memories} alt="memories" heigth="6" maxwidth="10"/>
+            <AppBar className={classes.appBar} pisition="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60" maxwidth="10"/>
             </AppBar>
             <Grow in>
                 <Container>
