50024ebf3aca1b92cf53f9bd864a3afb6ef68398
mateusgomes01 <mgm6@cin.ufpe.br>
2021-01-28 21:53:06 -0300

    finishes create a post

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -16,3 +16,13 @@ export const getPosts = () => async(dispatch) => {
     //const action = { type: 'FETCH_ALL', payload: [] }
 
 }
+
+export const createPost = (post) => async (dispatch) => {
+    try{
+        const { data } = await api.createPost(post);
+
+        dispatch({ type: 'CREATE', payload: data });
+    } catch (error) {
+        console.log(error);
+    }
+}
